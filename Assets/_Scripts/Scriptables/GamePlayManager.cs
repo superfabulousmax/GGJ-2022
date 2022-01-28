72062f59c663c9ebdc173ca8801f2e71b6d33b37
superfabulousmax <sinzara611@gmail.com>
2022-01-28 19:41:54 +0000

    Add URP and more asmdefs
    Add test GamePlayManager

---
+++ Assets/_Scripts/Scriptables/GamePlayManager.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using NaughtyAttributes;
+using yaSingleton;
+using Random = UnityEngine.Random;
+
+[CreateAssetMenu(fileName = "GameManager", menuName = "Singletons/GameManager")]
+public class GamePlayManager : Singleton<GamePlayManager>
+{
+
+}
