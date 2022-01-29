cd1c87170e0b0bfc142f5bacf9ea0ddaed7fc803
PseudoCore <59315135+PseudoCore@users.noreply.github.com>
2022-01-29 14:02:25 +0000

    Add enemy controller that seeks player and keeps distance between other enemies

---
+++ Assets/_Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private float _moveSpeed = 2f;
+    private float _separationSpeed = 1f;
+    private Transform _playerTarget;
+    private Rigidbody2D _rigidBody;
+    private const float OffsetToPlayer = 0.5f;
+    private const float OffsetToEnemy = 1f;
+
+    void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    public void SpawnAndSeek(Vector3 spawnPosition, Transform playerTransform)
+    {
+        transform.position = spawnPosition;
+        _playerTarget = playerTransform;
+    }
+
+    public void StopSeeking()
+    {
+        _playerTarget = null;
+    }
+
+    void FixedUpdate()
+    {
+        if (_playerTarget)
+        {
+            Vector2 distance = _playerTarget.position - transform.position;
+            Vector2 direction = distance.normalized;
+
+            if (distance.magnitude >= OffsetToPlayer)
+            {
+                _rigidBody.MovePosition(_rigidBody.position + _moveSpeed * direction * Time.fixedDeltaTime);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.TryGetComponent(out PlayerMovementController playerController))
+        {
+            Debug.Log("player touched");
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.TryGetComponent(out EnemyController enemyController))
+        {
+            Vector2 distance = transform.position - collision.transform.position;
+            float magnitude = distance.magnitude;
+            if (magnitude < OffsetToEnemy)
+            {
+                //_rigidBody.MovePosition(_rigidBody.position + _separationSpeed * distance.normalized * Time.fixedDeltaTime);
+                transform.Translate(distance.normalized * _separationSpeed * Time.fixedDeltaTime);
+            }
+        }
+    }
+}
--- Assets/_Scripts/Scriptables/GamePlayManager.cs
+++ Assets/_Scripts/Scriptables/GamePlayManager.cs
@@ -11,8 +11,12 @@ using Random = UnityEngine.Random;
 [CreateAssetMenu(fileName = "GameManager", menuName = "Singletons/GameManager")]
 public class GamePlayManager : yaSingleton.Singleton<GamePlayManager>
 {
-    // Player
-    private GameObject player;
+    private GameObject _playerPrefab;
+    private GameObject _enemyPrefab;
+
+    private GameObject _player;
+    private EnemyManager _enemyManager;
+
     // Units
     // Events
     public event Action sampleEvent = () => { };
@@ -25,13 +29,32 @@ public class GamePlayManager : yaSingleton.Singleton<GamePlayManager>
     {
         base.Initialize();
         Debug.Log("Initializing the GamePlayManager");
-        player = Resources.Load<GameObject>("Prefabs/Player");
-        Instantiate(player);
+        _playerPrefab = Resources.Load<GameObject>("Prefabs/Player");
+        _enemyPrefab = Resources.Load<GameObject>("Prefabs/Enemy");
+
         // Initialization code
+        _player = Instantiate(_playerPrefab);
+        SetupEnemy();
     }
 
     protected override void Deinitialize()
     {
         base.Deinitialize();
+    }
+
+    private void SetupEnemy()
+    {
+        _enemyManager = new EnemyManager(_player, _enemyPrefab);
+
+        // Example spawning
+        _enemyManager.Spawn(new Vector2(4f, 0));
+        _enemyManager.Spawn(new Vector2(-4f, 0));
+        _enemyManager.Spawn(new Vector2(0, 4f));
+        _enemyManager.Spawn(new Vector2(0, -4f));
+
+        _enemyManager.Spawn(new Vector2(2f, 2f));
+        _enemyManager.Spawn(new Vector2(-2f, 2f));
+        _enemyManager.Spawn(new Vector2(2f, -2f));
+        _enemyManager.Spawn(new Vector2(-2f, -2f));
     }
 }
