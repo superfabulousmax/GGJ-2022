233f5280abccfdef06e9150dfc96ee476d4028fc
PseudoCore <59315135+PseudoCore@users.noreply.github.com>
2022-01-29 17:46:27 +0000

    Adding enemy frame loop and assets

---
+++ Assets/_Scripts/Enemy/EnemySpriteLoop.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpriteLoop : MonoBehaviour
+{
+    [SerializeField] private Sprite[] _frameArray;
+    private int _currFrame;
+    private float _time;
+    private float _fps = 1f/30f;
+    private SpriteRenderer _spriteRenderer;
+
+    void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _currFrame = Random.Range(0, _frameArray.Length);
+        _spriteRenderer.sprite = _frameArray[_currFrame];
+    }
+
+    void Update()
+    {
+        _time += Time.deltaTime;
+
+        if (_time >= _fps)
+        {
+            _time -= _fps;
+            _currFrame = (_currFrame + 1) % _frameArray.Length;
+            _spriteRenderer.sprite = _frameArray[_currFrame];
+        }
+    }
+}
